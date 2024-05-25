f9d1b29074ba826df3690fb9324dc371ae9b8ae1
EnzoMedej <enzo.guimaraes@sptech.school>
2024-05-25 14:04:55 -0300

    atualizando test

---
+++ src/test/java/sptech/school/exemplo_actions/CalcuçadoraServiceTest.java
@@ -0,0 +1,21 @@
+package sptech.school.exemplo_actions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import sptech.school.exemplo_actions.service.CalculadoraService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalcuçadoraServiceTest {
+    @Test
+    @DisplayName("Quando acionado com 10 e 2, então deve retornar 5")
+    public void testDividir(){
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b = 2;
+        double esperado = 5;
+//vamo testando veyrrrrrrrrrrrrr 
+        double resultado = calculadoraService.dividir(a, b);
+        assertEquals(esperado, resultado);
+    }
+}
